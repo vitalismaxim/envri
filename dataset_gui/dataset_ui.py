c217f03aab1f14bed744104b4d5be847089501a9
vitalismaxim <vitalismaxim@gmail.com>
2021-09-09 03:41:53 +0200

    dataset_gui

---
+++ dataset_gui/dataset_ui.py
@@ -0,0 +1,364 @@
+from utils import *
+import glob
+from ntpath import basename
+import tkinter as tk
+import importlib
+import importlib.util
+from shutil import copyfile
+from tkinter.filedialog import asksaveasfile, asksaveasfilename
+
+
+class DatasetIndex:
+
+    def __init__(self):
+
+        # Init Vars
+        self.window = tk.Tk()
+        self.window.title("Dataset Indexing")
+
+        self.window.iconbitmap('envri_icon.ico')
+        self.found_files = list
+        self.feature_match = dict
+        self.standard_features = list
+
+        self.metadata_directory = tk.StringVar()
+        self.save_directory = tk.StringVar()
+        self.feature_config_file = tk.StringVar()
+        #self.feature_config_file2 = tk.StringVar()
+        self.domain_vars_file = tk.StringVar()
+        self.files = tk.StringVar()
+        self.file_box = tk.Listbox()
+        self.number_of_files = tk.IntVar()
+        self.biggest_file = tk.StringVar()
+        self.mapping_status = tk.StringVar()
+        self.file_process_status = tk.StringVar()
+        self.rules = tk.StringVar()
+        self.rule_field = tk.StringVar()
+        self.anaee_files = tk.IntVar()
+        self.icos_files = tk.IntVar()
+        self.nsidr_files = tk.IntVar()
+        self.sdn_files = tk.IntVar()
+        self.mapping_threshold = tk.DoubleVar()
+        self.essential_vars_threshold = tk.DoubleVar()
+
+        self.anaee_list = []
+        self.icos_list = []
+        self.nsidr_list = []
+        self.sdn_list = []
+        self.full_list = []
+
+
+
+        # Default files
+        self.feature_config_file.set("envri_config.json")
+        self.domain_vars_file.set("domain_variables.json")
+        self.metadata_directory.set("No directory choosen")
+        self.save_directory.set("No directory choosen")
+        self.rules.set("rules")
+        self.submit_rules()
+
+        self.mapping_status.set('No mapping')
+        self.file_process_status.set('Processing not started')
+        self.rule_field.set('Submit rule file name')
+
+        self.anaee_files.set(0)
+        self.icos_files.set(0)
+        self.nsidr_files.set(0)
+        self.sdn_files.set(0)
+        self.mapping_threshold.set(0.0)
+        self.essential_vars_threshold.set(0.0)
+
+        self.window.columnconfigure([0, 1, 2, 3], minsize=20)
+        self.window.rowconfigure([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], minsize=20)
+
+
+        # Functionality: Buttons and Labels
+        ##########################################################################################################################################
+
+        # Metadata
+        self.metadata_button_open = tk.Button(self.window, text = "Choose folder to index", command = self.choose_folder_metadata)
+        self.metadata_label2 = tk.Label(self.window, text = "Metadata directory: ", font = 'bold')
+        self.metadata_label = tk.Label(self.window, textvariable = self.metadata_directory)
+
+        self.metadata_folder = tk.Label(self.window, textvariable = self.metadata_directory)
+
+        # Save location
+        self.button_save = tk.Button(self.window, text = "Choose save location", command = self.choose_folder_save)
+        self.save_folder2 = tk.Label(self.window, text = 'Save location: ', font = 'bold')
+        self.save_folder = tk.Label(self.window, textvariable = self.save_directory)
+
+        # Config file
+        self.config_button_download = tk.Button(self.window, text = "Config file Download", command = self.save_config)
+        self.config_button_upload = tk.Button(self.window, text = "Config file Upload", command = self.choose_folder_config)
+        self.config_file_label2 = tk.Label(self.window, textvariable = self.feature_config_file)
+        self.config_file_label = tk.Label(self.window, text = "Config file loaded:", font = 'bold')
+        
+        # Domain vars
+        self.domain_button_download = tk.Button(self.window, text = "Domain variables Download", command = self.save_domain)
+        self.domain_button_upload = tk.Button(self.window, text = "Domain variables Upload", command = self.choose_folder_domain)
+        self.domain_file2 = tk.Label(self.window, text = 'Domain variables loaded:', font = 'bold')
+        self.domain_file = tk.Label(self.window, textvariable = self.domain_vars_file)
+
+        # rules
+        self.rule_button_download = tk.Button(self.window, text = 'Rules Download', command = self.save_rules)
+        self.rule_button_upload = tk.Button(self.window, text = 'Rules Upload', command = self.choose_rules)
+        self.rule_entry2 = tk.Label(self.window, text = "Rules loaded:", font = 'bold')
+        self.rule_entry = tk.Label(self.window, textvariable = self.rules)
+
+        # Files
+        self.found_files = tk.Label(self.window, textvariable = self.files)
+        self.count_files2 = tk.Label(self.window, text = 'Number of files found', font = 'bold')
+        self.count_files = tk.Label(self.window, textvariable = self.number_of_files)
+
+        # File split and breakdown count
+        self.file_split_label = tk.Label(self.window, text = "Files per source:", font = 'bold')
+
+        self.anaee_count2 = tk.Label(self.window, text = 'Anaee')
+        self.icos_count2 = tk.Label(self.window, text = 'Icos')
+        self.nsidr_count2 = tk.Label(self.window, text = 'Nsidr')
+        self.sdn_count2 = tk.Label(self.window, text = 'SeaDataNet')
+
+        self.anaee_count = tk.Label(self.window, textvariable = self.anaee_files)
+        self.icos_count = tk.Label(self.window, textvariable = self.icos_files)
+        self.nsidr_count = tk.Label(self.window, textvariable = self.nsidr_files)
+        self.sdn_count = tk.Label(self.window, textvariable = self.sdn_files)
+
+        # Mapping
+        self.create_mapping = tk.Button(self.window, text = 'Create mapping for metadata', command = self.extract_and_map)
+        self.status = tk.Label(self.window, textvariable = self.mapping_status)
+
+        self.process = tk.Button(self.window, text = 'Process metadata', command = self.process_files2)
+        self.process_status = tk.Label(self.window, textvariable = self.file_process_status)
+
+        self.mapping_threshold_label = tk.Label(self.window, text = "Mapping strength threshold:", font = 'bold')
+        self.mapping_threshold_slider = tk.Scale(self.window, from_ = 0, to = 100, orient = 'horizontal', variable = self.mapping_threshold)
+
+        self.essential_vars_label = tk.Label(self.window, text = "Domain variables treshold:", font = 'bold')
+        self.essential_vars_slider = tk.Scale(self.window, from_ = 0, to = 100, orient = 'horizontal', variable = self.essential_vars_threshold)
+
+
+
+        # Placement
+        ######################################################################################################################################
+
+        # Buttons
+        self.config_button_download.grid(row=0, column=0, padx=5, pady=5)
+        self.config_button_upload.grid(row=1, column=0, padx=5, pady=5)
+
+        self.domain_button_download.grid(row=0, column=1, padx=5, pady=5)
+        self.domain_button_upload.grid(row=1, column=1, padx=5, pady=5)
+
+        self.rule_button_download.grid(row=0, column=2, padx=5, pady=5)
+        self.rule_button_upload.grid(row=1, column=2, padx=5, pady=5)
+        
+
+        self.button_save.grid(row=8, column=0, padx=5, pady=5)
+        self.save_folder2.grid(row=8, column=1, padx=5, pady=5)
+        self.save_folder.grid(row=9, column=1, padx=5, pady=5)
+
+        self.metadata_button_open.grid(row=6, column=0, padx=5, pady=5)
+        
+       
+        # Labels
+        self.metadata_label2.grid(row=6, column=1, padx=5, pady=5)
+        self.metadata_label.grid(row=7, column=1, padx=5, pady=5)
+        
+        self.config_file_label.grid(row=2, column=0, padx=5, pady=5)
+        self.config_file_label2.grid(row=3, column=0, padx=5, pady=5)
+        self.domain_file2.grid(row=2, column=1, padx=5, pady=5)
+        self.domain_file.grid(row=3, column=1, padx=5, pady=5)
+        self.rule_entry2.grid(row=2, column=2, padx=5, pady=5)
+        self.rule_entry.grid(row=3, column=2, padx=5, pady=5)
+
+        #self.file_box.grid(row=7, column=0, padx=5, pady=5)
+        self.count_files2.grid(row=6, column=2, padx=5, pady=5)
+        self.count_files.grid(row=7, column=2, padx=5, pady=5)
+
+        self.create_mapping.grid(row=16, column=1, padx=5, pady=5)
+        self.status.grid(row=17, column=1, padx=5, pady=5)
+
+        self.mapping_threshold_label.grid(row=16, column=0, padx=5, pady=5)
+        self.mapping_threshold_slider.grid(row=17, column=0, padx=5, pady=5)
+
+        self.essential_vars_label.grid(row=16, column=2, padx=5, pady=5)
+        self.essential_vars_slider.grid(row=17, column=2, padx=5, pady=5)
+
+        # Split
+        self.file_split_label.grid(row=10, column=0, padx=5, pady=5)
+
+        self.anaee_count2.grid(row=11, column=0, padx=5, pady=5)
+        self.icos_count2.grid(row=11, column=1, padx=5, pady=5)
+        self.nsidr_count2.grid(row=11, column=2, padx=5, pady=5)
+        self.sdn_count2.grid(row=11, column=3, padx=5, pady=20)
+
+        self.anaee_count.grid(row=12, column=0, padx=5, pady=5)
+        self.icos_count.grid(row=12, column=1, padx=5, pady=5)
+        self.nsidr_count.grid(row=12, column=2, padx=5, pady=5)
+        self.sdn_count.grid(row=12, column=3, padx=5, pady=20)
+
+        # Processing
+        self.process.grid(row=16, column=3, padx=5, pady=5)
+        self.process_status.grid(row=17, column=3, padx=5, pady=5)
+        
+
+        # TK
+        self.window.mainloop()
+
+
+    def split_source(self, file_list):
+        anaee_list = []
+        icos_list = []
+        nsidr_list = []
+        sdn_list = []
+        for file in file_list:
+            data = open_file(file)
+            values = list(iterate_all(data, 'value'))
+            for value in values:
+                if value is not None and type(value) is str:
+                    if 'seadatanet' in value:
+                        sdn_list.append(file)
+                        break
+                    elif 'icos' in value:
+                        icos_list.append(file)
+                        break
+                    elif 'anaee' in value:
+                        anaee_list.append(file)
+                        break
+                    elif 'DigitalSpecimen' in value:
+                        nsidr_list.append(file)
+                        break
+            #print(file)
+        return anaee_list, icos_list, nsidr_list, sdn_list
+
+    def choose_folder_metadata(self):
+        self.metadata_directory.set(askdirectory())
+        file_path = self.metadata_directory.get() + '/*.json'
+        self.found_files = glob.glob(file_path)
+        #self.files = tk.Listbox(self.window, listvariable = self.found_files, height = 20, width = 200, selectmode = 'extended')
+        self.number_of_files.set(len(self.found_files))
+        self.anaee_list, self.icos_list, self.nsidr_list, self.sdn_list = self.split_source(self.found_files)
+        self.anaee_files.set(len(self.anaee_list))
+        self.icos_files.set(len(self.icos_list))
+        self.nsidr_files.set(len(self.nsidr_list))
+        self.sdn_files.set(len(self.sdn_list))
+        self.full_list = [self.anaee_list, self.icos_list, self.nsidr_list, self.sdn_list]
+
+    # Download files
+    def save_config(self):
+        files = [('JSON Config File', '*.json')]
+        write_file(asksaveasfilename(filetypes = files, defaultextension = files), open_file(self.feature_config_file.get()))
+        
+    def save_domain(self):
+        files = [('JSON Domain Variables', '*.json')]
+        write_file(asksaveasfilename(filetypes = files, defaultextension = files), open_file(self.domain_vars_file.get()))
+
+    def save_rules(self):
+        files = [('Python rule script', '*.py')]
+        copyfile(self.rules.get() + '.py', asksaveasfilename(filetypes = files, defaultextension = files))
+
+    # Upload files
+    def choose_folder_config(self):
+        self.feature_config_file.set(askopenfilename(filetypes=[("JSON", "*.json")]))
+
+    def choose_folder_domain(self):
+        self.domain_vars_file.set(askopenfilename(filetypes=[("JSON", "*.json")]))
+
+    def choose_rules(self):
+        rule_file = (askopenfilename(filetypes=[("Python rule script", "*.py")]))
+        self.rules.set(basename(rule_file[:-3]))
+
+    def choose_folder_save(self):
+        self.save_directory.set(askdirectory())
+    
+    def changeText(self):
+        self.metadata_directory.set("Updated Text")
+
+    def submit_rules(self):
+        self.rule_field.set('Rules submitted!')
+        self.rule_file = importlib.import_module(self.rules.get())
+
+    def extract_and_map(self):
+        
+        self.mapping_status.set('Creating mapping...')
+        feature_syn_dict = open_file(self.feature_config_file.get())
+        feature_syn_list = [item for sublist in list(feature_syn_dict.values()) for item in sublist]
+        self.standard_features = feature_syn_dict.keys()
+        self.mapping = []
+
+        for source in self.full_list:
+            biggest_file = biggest(source)
+            data = open_file(biggest_file)
+            key_list = key_zip(data)
+            dict_of_keys = key_dict(data)
+            all_matches = mapper(feature_syn_list, dict_of_keys)
+
+            feature_match = {}
+            for key in all_matches:
+                best_match = max(all_matches[key], key = all_matches[key].get)
+                sim_score = all_matches[key][best_match]
+                best_key = dict_of_keys[best_match]
+                feature_match[key] = [best_key, sim_score]
+
+            condensed_dict = {}
+            for key, values in feature_syn_dict.items():
+                condensed_dict[key] = [None, 0]
+                for value in values:
+                    if feature_match[value][1] > condensed_dict[key][1] and feature_match[value][1] <= 1 and feature_match[value][1] >= (self.mapping_threshold.get() / 100):
+                        condensed_dict[key] = feature_match[value]
+            feature_match = condensed_dict
+            self.feature_match = feature_match
+
+            self.mapping.append(feature_match)
+        self.mapping_status.set('Mapping complete!')
+        print(self.mapping)
+        print(len(self.mapping))
+        print(self.mapping_threshold.get())
+
+    def process_files2(self):
+        self.file_process_status.set('Processing files...')
+        directory = self.save_directory.get()
+
+        for i in range(len(self.full_list)):
+            for file in self.full_list[i]:
+                data = open_file(file)
+                value_dict = dict.fromkeys(self.standard_features, "N/A")
+                for key in value_dict.keys():
+                    content = list(findkeys(data, self.mapping[i][key][0]))
+                    value_dict[key] = content
+
+                value_dict = self.rule_file.run_funcs(value_dict)
+                value_dict = topic_miner(value_dict)
+                value_dict = domain(value_dict, self.essential_vars_threshold.get())
+                write_file(directory + '/' + basename(file) + ".json", value_dict)
+        self.file_process_status.set('Processing complete!')
+        
+        return
+
+    def process_files(self):
+        self.file_process_status.set('Processing files...')
+        directory = self.save_directory.get()
+        
+        for file in self.found_files:
+            data = open_file(file)
+            value_dict = dict.fromkeys(self.standard_features, "N/A")
+            for key in value_dict.keys():
+                content = list(findkeys(data, self.feature_match[key][0]))
+                value_dict[key] = content
+
+            value_dict = self.rule_file.run_funcs(value_dict)
+            value_dict = topic_miner(value_dict)
+            value_dict = domain(value_dict, self.essential_vars_threshold.get())
+            write_file(directory + '/' + value_dict['identifier'] +'.json', value_dict)
+        self.file_process_status.set('Processing complete!')
+        
+
+DatasetIndex()
+
+
+
+
+
+
+
+
+
---
+++ dataset_gui/utils.py
@@ -0,0 +1,287 @@
+import glob
+import os
+import uuid
+import spacy
+from spacy.lang.en import English
+import json
+import gensim
+from nltk.corpus import wordnet as wn
+from gensim.utils import simple_preprocess
+from gensim import corpora
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+import numpy as np
+import nltk
+import tkinter as tk
+from tkinter.filedialog import askdirectory, askopenfilename
+import random
+#from utils import cleaner
+#from rules import *
+import importlib
+
+
+#nltk.download('stopwords')
+parser = English()
+en_stop = set(nltk.corpus.stopwords.words('english'))
+
+
+
+# Semantic language model
+nlp = spacy.load('en_core_web_lg')
+
+
+def cleaner(lst):
+    cleaned_list = []
+    for feature in lst:
+        #print(feature)
+        if ":" in feature:
+            clean = re.split(':(.*)', feature)
+            if len(clean) > 1:
+                clean = clean[1]
+            else:
+                clean = clean[0][1:]
+        else:
+            clean = feature
+        
+        #clean = clean.replace("_", " ")
+        clean = re.sub("([a-z])([A-Z])","\g<1> \g<2>",clean)
+        clean = clean.lower()
+        clean = ''.join([i for i in clean if i.isalpha() or i.isspace()])
+        if len(clean) <= 3:
+            cleaned_list.append(clean)
+        elif clean[2] == '_':
+            #print(feature, clean[3:])
+            cleaned_list.append(clean[3:])
+        else:
+            cleaned_list.append(clean)
+        
+    return cleaned_list
+
+
+def findkeys(node, kv):
+    if isinstance(node, list):
+        for i in node:
+            for x in findkeys(i, kv):
+                yield x
+    elif isinstance(node, dict):
+        if kv in node:
+            yield node[kv]
+        for j in node.values():
+            for x in findkeys(j, kv):
+                yield x
+
+def write_file(write_path, metadict):
+    with open(write_path, 'w') as f:
+        json.dump(metadict, f, indent=1, ensure_ascii=False)
+
+    
+def write_file(write_path, metadict):
+    with open(write_path, 'w') as f:
+        json.dump(metadict, f, indent=1, ensure_ascii=False)
+
+def open_file(read_path):
+    with open(read_path, "r", errors='ignore') as read_file:
+        data = json.load(read_file)
+    return data
+
+def recursive_items(dictionary):
+    for key, value in dictionary.items():
+        if type(value) is dict:
+            yield (key, value)
+            yield from recursive_items(value)
+        else:
+            yield (key, value)
+
+def key_dict(data):
+    keys = []
+    for key, value in recursive_items(data):
+        keys.append(key)
+    clean_keys = cleaner(keys)
+    key_dict= {}
+    for i in range(len(clean_keys)):
+        clean_key = clean_keys[i]
+        key = keys[i]
+        key_dict[clean_key] = key
+    return key_dict
+
+def key_zip(data):
+    keys = []
+    for key, value in recursive_items(data):
+        keys.append(key)
+    clean_keys = cleaner(keys)
+    return list(zip(clean_keys,keys))
+
+
+def mapper(feature_syn_dict, dict_of_keys):
+    all_matches = {}
+    for word in feature_syn_dict:
+        matches = {}
+        token1 = nlp(word)
+        for word2 in list(dict_of_keys):
+            token2 = nlp(word2)
+            matches[word2] = token1.similarity(token2)
+        all_matches[word] = matches
+    return all_matches
+
+
+def iterate_all(iterable, returned="key"):
+    if isinstance(iterable, dict):
+        for key, value in iterable.items():
+            if returned == "key":
+                yield key
+            elif returned == "value":
+                if not (isinstance(value, dict) or isinstance(value, list)):
+                    yield value
+            else:
+                raise ValueError("'returned' keyword only accepts 'key' or 'value'.")
+            for ret in iterate_all(value, returned=returned):
+                yield ret
+    elif isinstance(iterable, list):
+        for el in iterable:
+            for ret in iterate_all(el, returned=returned):
+                yield ret
+
+
+def tokenize(text):
+    lda_tokens = []
+    tokens = parser(text)
+    for token in tokens:
+        if token.orth_.isspace():
+            continue
+        elif token.like_url:
+            lda_tokens.append('URL')
+        elif token.orth_.startswith('@'):
+            lda_tokens.append('SCREEN_NAME')
+        else:
+            lda_tokens.append(token.lower_)
+    return lda_tokens
+
+def get_lemma(word):
+    lemma = wn.morphy(word)
+    if lemma is None:
+        return word
+    else:
+        return lemma
+
+def prepare_text_for_lda(text):
+    tokens = tokenize(text)
+    tokens = [token for token in tokens if len(token) > 4]
+    tokens = [token for token in tokens if token not in en_stop]
+    tokens = [get_lemma(token) for token in tokens]
+    return tokens
+
+def topic_miner(data):
+    value_list = []
+    text_data = []
+    topic_list = []
+    value_list.append(data['description'])
+    value_list.append(data['abstract'])
+    value_list.append(data['headline'])
+    for keyword in data['keyword']:
+        value_list.append(keyword)
+
+    for line in value_list:
+        tokens = prepare_text_for_lda(line)
+        text_data.append(tokens)
+
+    dictionary = corpora.Dictionary(text_data)
+    corpus = [dictionary.doc2bow(text) for text in text_data]
+    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = 10, id2word=dictionary, passes=50)
+
+    topics = ldamodel.print_topics(num_words=1)
+    for topic in topics:
+        topic_list.append(topic[1][7:-1])
+
+    data['info'] = list(set(topic_list))
+    return data
+
+
+domain_vars = open_file('C:/Users/xiemp/Documents/afstudeer/features/domain_variables.json')
+
+
+def domain(data, threshold = 0):
+    domain_variables = domain_vars['ocean']
+    value_list = []
+    value_list.append(data['description'])
+    value_list.append(data['abstract'])
+    value_list.append(data['headline'])
+    for keyword in data['keyword']:
+        value_list.append(keyword)
+
+    all_matches = mapper(value_list, domain_variables)
+    domain_match = {}
+    for key in all_matches:
+        best_match = max(all_matches[key], key = all_matches[key].get)
+        sim_score = all_matches[key][best_match]
+        domain_match[key] = [best_match, sim_score]
+
+    for key, value in domain_match.items():
+        if value[1] > threshold:
+            data['info'].append(value[0])
+
+    data['info'] = list(set(data['info']))
+    return data
+
+def biggest(files):
+    file_stats = {}
+    for file in files:
+        file_stats[file] = os.stat(file).st_size
+
+    #biggest file is most representive of keys in all metadata
+    biggest_file = max(file_stats, key=file_stats.get)
+    return biggest_file
+
+
+def choose_folder_metadata(self):
+    self.metadata_directory.set(askdirectory())
+    file_path = self.metadata_directory.get() + '/*.json'
+    self.found_files = glob.glob(file_path)
+    #print('location:', file_path)
+    #print('files:', found_files)
+    for file in self.found_files:
+        self.file_box.insert(-1, file)
+    
+    self.files = tk.Listbox(self.window, listvariable = self.found_files, height = 20, width = 200, selectmode = 'extended')
+    self.number_of_files.set(len(self.found_files))
+    self.biggest_file = biggest(self.found_files)
+    
+def choose_folder_save(self):
+    self.save_directory.set(askdirectory())
+
+def choose_folder_config(self):
+    self.feature_config_file.set(askopenfilename(
+    filetypes=[("JSON", "*.json")]))
+    
+def choose_folder_domain(self):
+    self.domain_vars_file.set(askopenfilename(
+    filetypes=[("JSON", "*.json")]))
+
+def changeText(self):
+    self.metadata_directory.set("Updated Text")
+
+def extract_and_map(self, data, config_file):
+    feature_syn_dict = open_file(config_file)
+    feature_syn_list = [item for sublist in list(feature_syn_dict.values()) for item in sublist]
+    data = open_file(self.biggest_file)
+    key_list = key_zip(data)
+    dict_of_keys = key_dict(data)
+    all_matches = mapper(feature_syn_list, dict_of_keys)
+
+    feature_match = {}
+    for key in all_matches:
+        best_match = max(all_matches[key], key = all_matches[key].get)
+        sim_score = all_matches[key][best_match]
+        best_key = dict_of_keys[best_match]
+        feature_match[key] = [best_key, sim_score]
+
+    condensed_dict = {}
+    for key, values in feature_syn_dict.items():
+        condensed_dict[key] = [None, 0]
+        for value in values:
+            if feature_match[value][1] > condensed_dict[key][1] and feature_match[value][1] <= 1:
+                condensed_dict[key] = feature_match[value]
+    feature_match = condensed_dict
+    return feature_match
+    
+     
